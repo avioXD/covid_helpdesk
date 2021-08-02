fe48cd20626345a6d5b4a5f13d287bd6fa19f832
ABHISHEK DAS <67656730+avioXD@users.noreply.github.com>
2021-08-02 21:20:23 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: true,
   firebaseConfig:{
-    apiKey: "AIzaSyAb7LvBirLrs_dd4KQPt7ObfEmXIcPuOXE",
+    apiKey: "***************************",
   authDomain: "covidproject-9bc03.firebaseapp.com",
   projectId: "covidproject-9bc03",
   storageBucket: "covidproject-9bc03.appspot.com",
   messagingSenderId: "316478521587",
-  appId: "1:316478521587:web:ee3737ce545bd76302f786",
-  measurementId: "G-6HNKGP0MNZ"
+  appId: "1:316478521587:web:ee3*******************",
+  measurementId: "***********"
   }
 };
 
